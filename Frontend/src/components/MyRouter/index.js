c1daffc1ca43ffec6c80a063d2be41bee4b2c41e
acartiermichaud <a.cartier.michaud@gmail.com>
2023-12-17 10:53:13 +0100

    setup gh-pages

--- Frontend/src/components/MyRouter/index.js
+++ Frontend/src/components/MyRouter/index.js
@@ -14,7 +14,7 @@ import Footer from '../../components/Footer'
 function MyRouter () {
   
   return (
-    <Router basename="https://acartiermichaud.github.io/p11/">
+    <Router basename="https://acartiermichaud.github.io/p11">
       <Routes>
         <Route path="/" element={<Home />} errorElement={<Error />}/>
         <Route path="/login" element={<Login />} errorElement={<Error />}/>
